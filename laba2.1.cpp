c98d808e3cc2206379d5208fcc548af73045b82b
Tonya <tonya.kluka@gmail.com>
2023-09-28 22:12:07 +0300

    laba2

---
+++ laba2.1.cpp
@@ -0,0 +1,30 @@
+// Lab_02.cpp
+	// Клюка Антоніна
+	// Лабораторна робота № 2.
+	// Лінійні програми.
+	// Варіант 12
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double x;
+	double z1;
+	double z2;
+
+	const double PI = 3.14;
+
+	cout << "Angle Alpha: "; cin >> x;
+
+	z1 = sin(4 * x) / (1 + cos(4 * x)) * cos(2 * x) / (1 + cos(2 * x));
+	z2 = cos((3 * PI / 2) - x) / sin((3 * PI / 2) - x);
+
+	cout << endl;
+
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+
+	return 0;
+}
